934f84a40a0d520c533ef4bb93e9ef844400962e
Matthew_Heyert <heyerthb@gmail.com>
2019-06-12 09:31:56 -0700

    challenge 7 question 4

--- server.js
+++ server.js
@@ -13,29 +13,29 @@ const app = express();
 app.use(cors());
 
 app.get('/location', (request, response) => {
-    try {
-        const locationData = searchToLatLong(request.query.data);
-        response.send(locationData);
-    }
-    catch (error) {
-        console.error(error);
-        response.status(500).send('Status: 500. So sorry, something went wrong.');
-    }
+  try {
+    const locationData = searchToLatLong(request.query.data);
+    response.send(locationData);
+  }
+  catch (error) {
+    console.error(error);
+    response.status(500).send('Status: 500. So sorry, something went wrong.');
+  }
 });
 
 app.get('/weather', (request, response) => {
-    try {
-        const weatherData = searchWeatherData(request.query.data);
-        
-        response.send(weatherData);
-    }
-    catch (error) {
-        
-        console.error(error);
-        
+  try {
+    const weatherData = searchWeatherData(request.query.data);
 
-        response.status(500).send('Status: 500. So sorry, something went wrong.');
-    }
+    response.send(weatherData);
+  }
+  catch (error) {
+
+    console.error(error);
+
+
+    response.status(500).send('Status: 500. So sorry, something went wrong.');
+  }
 
 });
 
@@ -49,31 +49,31 @@ app.get('/weather', (request, response) => {
 //     this.longitude = geoData.location.lng;
 // }
 function searchWeatherData(query) {
-    const skyData = require('./data/darksky.json')
-    const weather = {
-        search_query: query,
-        time: skyData.daily.data[0].time,
-        forecast: skyData.daily.data[0].summary,
-        time2: skyData.daily.data[1].time,
-        forecast2: skyData.daily.data[1].summary
+  const skyData = require('./data/darksky.json')
+  const weather = {
+    search_query: query,
+    time: skyData.daily.data[0].time,
+    forecast: skyData.daily.data[0].summary,
+    time2: skyData.daily.data[1].time,
+    forecast2: skyData.daily.data[1].summary
 
-        
-        // latitude: skyData.results[0],
-        // longitude: skyData.results[0],
-    };
-    return weather;
+
+    // latitude: skyData.results[0],
+    // longitude: skyData.results[0],
+  };
+  return weather;
 }
 
 
 function searchToLatLong(query) {
-    const geoData = require('./data/geo.json')
-    const location = {
-        search_query: query,
-        formatted_query: geoData.results[0].formatted_address,
-        latitude: geoData.results[0].geometry.location.lat,
-        longitude: geoData.results[0].geometry.location.lng
-    };
-    return location;
+  const geoData = require('./data/geo.json')
+  const location = {
+    search_query: query,
+    formatted_query: geoData.results[0].formatted_address,
+    latitude: geoData.results[0].geometry.location.lat,
+    longitude: geoData.results[0].geometry.location.lng
+  };
+  return location;
 }
 
 //   const location = new Location(request.query.data, )
